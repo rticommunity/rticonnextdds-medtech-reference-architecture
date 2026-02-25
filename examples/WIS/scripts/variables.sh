7b61818a96cdbc6ad2bf6a29fa9c7b3e2b44ce7d
Andy Hall <ahall@rti.com>
2026-02-25 16:53:48 +0100

    Updated the WIS variables script to make it more portable and added a POSIX compliant option

--- examples/WIS/scripts/variables.sh
+++ examples/WIS/scripts/variables.sh
@@ -5,6 +5,8 @@
 # export PUBLIC_PORT=<PUBLIC_PORT> # Public port that the Web Integration Service will listen on.
 # export DOC_ROOT=<DOC_ROOT>       # Document root for the Web Integration Service.
 
-export NDDSHOME=~/rti_connext_dds-7.3.0
-export PUBLIC_PORT=80
+
 export PUBLIC_PORT=8080
+export DOC_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+#if not using bash, change the above line to:
+#export DOC_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
